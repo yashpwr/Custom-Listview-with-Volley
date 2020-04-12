6dc107c9e515b0bd6c1d8e3c49f820ab02b9ef95
yash <yashdvn@gmail.com>
2020-04-12 12:37:14 +0530

    initial commit

---
+++ app/src/main/java/com/codedecode/customlistviewvolley/util/LruBitmapCache.java
@@ -0,0 +1,37 @@
+package com.codedecode.customlistviewvolley.util;
+
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+public class LruBitmapCache extends LruCache<String, Bitmap> implements
+		ImageCache {
+	private static int getDefaultLruCacheSize() {
+		final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+		return maxMemory / 8;
+	}
+
+	public LruBitmapCache() {
+		this(getDefaultLruCacheSize());
+	}
+
+	private LruBitmapCache(int sizeInKiloBytes) {
+		super(sizeInKiloBytes);
+	}
+
+	@Override
+	protected int sizeOf(String key, Bitmap value) {
+		return value.getRowBytes() * value.getHeight() / 1024;
+	}
+
+	@Override
+	public Bitmap getBitmap(String url) {
+		return get(url);
+	}
+
+	@Override
+	public void putBitmap(String url, Bitmap bitmap) {
+		put(url, bitmap);
+	}
+}
